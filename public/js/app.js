37a472c17e085abec9ad23290958342fe5453304
Daryl Jewkes <github@daryljewkes.com>
2015-12-08 21:51:44 +0000

    code review

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,8 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'MovieCtrl', 'MovieService', 'GeekCtrl', 'GeekService', 'ui.bootstrap', 'xeditable', 'chart.js'])
+angular.module('meanApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'MovieCtrl', 'MovieService', 'GeekCtrl', 'GeekService', 'ui.bootstrap', 'xeditable', 'chart.js'])
 .controller('HeaderController', function($scope, $location) {
   $scope.isActive = function (viewLocation) {
     return $location.path().indexOf(viewLocation) >= 0;
   };
-
-  $scope.isCollapsed = true;
-
-  // $scope.isCollapsed = true;
- //  function NavBarCtrl($scope) {
- //  	console.log('collapsing')
- //    $scope.isCollapsed = true;
-	// }
-
- //  $scope.toggleCollapse = function() {
- //  	console.log('clicked')
- //  	$('.navbar-collapse').click(function() {
-	//   $('.navbar-collapse').collapse('hide');
-	// });
- //  }
 }).controller('FooterController', function($scope, $location) {
 
 })
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';	
-	
+	$scope.tagline = "That's no moon. It's a space station.";	
+
 	$scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
 	  $scope.series = ['Series A', 'Series B'];
 
