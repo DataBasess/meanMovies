86729b9177bad8ca9b7aabdb80046fe39aa5dd26
Daryl Jewkes <github@daryljewkes.com>
2015-12-08 16:32:06 +0000

    prepping for charts

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'MovieCtrl', 'MovieService', 'GeekCtrl', 'GeekService', 'ui.bootstrap', 'xeditable'])
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'MovieCtrl', 'MovieService', 'GeekCtrl', 'GeekService', 'ui.bootstrap', 'xeditable', 'chart.js'])
 .controller('HeaderController', function($scope, $location) {
   $scope.isActive = function (viewLocation) {
     return $location.path().indexOf(viewLocation) >= 0;
