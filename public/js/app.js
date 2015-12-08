cb8a0c967435b5f1fe2dc02779b90a5ad79bba74
Daryl Jewkes <github@daryljewkes.com>
2015-12-08 09:46:59 +0000

    added collapsible menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService', 'ui.bootstrap'])
+.controller('HeaderController', function($scope, $location) {
+  $scope.isActive = function (viewLocation) {
+    return $location.path().indexOf(viewLocation) >= 0;
+  };
+
+  $scope.isCollapsed = true;
+
+  // $scope.isCollapsed = true;
+ //  function NavBarCtrl($scope) {
+ //  	console.log('collapsing')
+ //    $scope.isCollapsed = true;
+	// }
+
+ //  $scope.toggleCollapse = function() {
+ //  	console.log('clicked')
+ //  	$('.navbar-collapse').click(function() {
+	//   $('.navbar-collapse').collapse('hide');
+	// });
+ //  }
+})
+
